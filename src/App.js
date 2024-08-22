d34ca3560751a2f27d18d0a5cf0199a3836cf87a
daj3on9 <dajung5623@naver.com>
2024-08-22 17:59:24 +0900

    ✨ Feat : 로그인 관련 설정 추가

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React from "react";
+import React from 'react';
 
-import Home from "./page/Home";
-import ShopInsert from "./page/ShopInsert";
-import MenuInsert from "./page/MenuInsert";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './page/Home';
+import ShopInsert from './page/ShopInsert';
+import MenuInsert from './page/MenuInsert';
+import Login from './page/auth/Login';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" Component={Home} />
-        <Route path="/menuinsert" Component={MenuInsert} />
-        <Route path="/shopinsert" Component={ShopInsert} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" Component={Home} />
+                <Route path="/login" Component={Login} />
+                <Route path="/menuinsert" Component={MenuInsert} />
+                <Route path="/shopinsert" Component={ShopInsert} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/store/User.js
@@ -0,0 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const userSlice = createSlice({
+    name: 'userToken',
+    initialState: {
+        studentName: null,
+    },
+    reducers: {
+        SET_USER: (state, action) => {
+            state.studentName = action.payload.studentName;
+        },
+        DELETE_USER: (state) => {
+            state.studentName = null;
+        },
+    },
+});
+
+export const { SET_USER, DELETE_USER } = userSlice.actions;
+
+export default userSlice.reducer;
