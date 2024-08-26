e6f179ae8afa4cc12bcfd3aaf378ddf1efda41df
daj3on9 <dajung5623@naver.com>
2024-08-26 18:42:52 +0900

    ✨ Feat : 로그인 url 수정 및 API 통신 코드 추가

--- src/page/auth/Login.js
+++ src/page/auth/Login.js
@@ -1,89 +1,112 @@
-import * as React from 'react';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import styled from 'styled-components';
+import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import styled from "styled-components";
 
-import logoImage from '../../assets/logo.png';
-import { useDispatch } from 'react-redux';
-import { postLogin } from '../../api/Users';
+import logoImage from "../../assets/logo.png";
+import { useDispatch } from "react-redux";
+import { postLogin } from "../../api/Users";
+import { useNavigate } from "react-router-dom";
+
+import { doLogout } from "../../api/Users";
 
 const defaultTheme = createTheme();
 
 const SubmitBtn = styled.button`
-    background-color: #06d1af;
-    border: none;
-    color: white;
-    width: -webkit-fill-available;
-    height: 40px;
-    border-radius: 5px;
-    margin-top: 30px;
+  background-color: #06d1af;
+  border: none;
+  color: white;
+  width: -webkit-fill-available;
+  height: 40px;
+  border-radius: 5px;
+  margin-top: 30px;
 `;
 
 export default function Login() {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    // 로그인
-    const handleSubmit = async (event) => {
-        event.preventDefault();
+  const [userData, setUserData] = React.useState({
+    email: "",
+    password: "",
+  });
 
-        const data = new FormData(event.currentTarget);
-        const userData = {
-            id: data.get('id'),
-            password: data.get('password'),
-        };
+  // 로그인
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    console.log(userData);
 
-        const loginSuccess = await postLogin(userData, dispatch);
+    const loginSuccess = await postLogin(userData, dispatch);
 
-        if (loginSuccess) {
-            alert('성공');
-        }
-    };
+    if (loginSuccess) {
+      alert("성공");
+      navigate("/");
+    }
+  };
 
-    return (
-        <ThemeProvider theme={defaultTheme}>
-            <Container component="main" maxWidth="xs">
-                <CssBaseline />
-                <Box
-                    sx={{
-                        marginTop: 8,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                    }}
-                >
-                    <img src={logoImage} alt="logo" style={{ width: '8rem' }}></img>
-                    <Typography component="h1" variant="h5">
-                        <b>LOGIN</b>
-                    </Typography>
-                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="id"
-                            label="아이디"
-                            name="id"
-                            autoComplete="id"
-                            autoFocus
-                        />
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="password"
-                            label="비밀번호"
-                            type="password"
-                            id="password"
-                            autoComplete="current-password"
-                        />
-                        <SubmitBtn type="submit">로그인</SubmitBtn>
-                    </Box>
-                </Box>
-            </Container>
-        </ThemeProvider>
-    );
+  // 로그인 설정
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setUserData({ ...userData, [name]: value });
+  };
+
+  // 로그아웃
+  const handleLogOut = (event) => {
+    event.preventDefault();
+    doLogout(dispatch); // 토큰 삭제 -> 로그아웃
+  };
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}>
+          <img src={logoImage} alt="logo" style={{ width: "8rem" }}></img>
+          <Typography component="h1" variant="h5">
+            <b>LOGIN</b>
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="아이디"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              onChange={onChange}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="비밀번호"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange={onChange}
+            />
+            <SubmitBtn type="submit">로그인</SubmitBtn>
+            <SubmitBtn onClick={handleLogOut}>로그아웃</SubmitBtn>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
 }
---
+++ src/store/useStore.js
@@ -0,0 +1,13 @@
+import { useSelector } from "react-redux";
+
+// acessToken 반환
+export function useAccessToken() {
+  const accessToken = useSelector((state) => state.authToken.accessToken);
+  return accessToken;
+}
+
+// authenticated 반환 (로그인 여부)
+export function useAuthenticated() {
+  const authenticated = useSelector((state) => state.authToken.authenticated);
+  return authenticated;
+}
